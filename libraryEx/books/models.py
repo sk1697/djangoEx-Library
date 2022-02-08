c17965571c05cf5ca0a1f90ee3a20fe0f6ba95ef
jrabbit.park <sk1697@naver.com>
2022-02-08 22:56:45 +0900

    user를 foreignkey로 등록하여 migrate

--- libraryEx/books/models.py
+++ libraryEx/books/models.py
@@ -1,12 +1,13 @@
 import datetime
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Book(models.Model):
     book_name = models.CharField(max_length=100)
     book_author = models.CharField(max_length=100)
     book_publisher = models.CharField(max_length=100)
+    book_borrower = models.ForeignKey(User,on_delete=models.PROTECT)
     #book_relDate = models.DateTimeField(default=datetime.datetime.now())
 
     def __str__(self):
--- libraryEx/libraryEx/settings.py
+++ libraryEx/libraryEx/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'books.apps.BooksConfig',
     'common.apps.CommonConfig',
+
 ]
 
 MIDDLEWARE = [
