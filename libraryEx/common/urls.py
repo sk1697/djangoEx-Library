c57bf9f6ede4b076fdf3879a867c1b7061fa486b
jrabbit.park <sk1697@naver.com>
2022-02-09 00:06:48 +0900

    common views를 import못해서 views.py파일 분리

--- libraryEx/common/urls.py
+++ libraryEx/common/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views
+# from . import views
+from .views import common_views
 
 app_name = 'common'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', views.signup, name='signup'),
+    path('signup/', common_views.signup, name='signup'),
 ]
