fb47e5becaa0cccfccd843a05b0a8364b9230502
jrabbit.park <sk1697@naver.com>
2022-02-09 01:16:01 +0900

    user model 삭제

--- libraryEx/books/models.py
+++ libraryEx/books/models.py
@@ -7,7 +7,6 @@ class Book(models.Model):
     book_name = models.CharField(max_length=100)
     book_author = models.CharField(max_length=100)
     book_publisher = models.CharField(max_length=100)
-    book_borrower = models.ForeignKey(User,on_delete=models.PROTECT)
     #book_relDate = models.DateTimeField(default=datetime.datetime.now())
 
     def __str__(self):
