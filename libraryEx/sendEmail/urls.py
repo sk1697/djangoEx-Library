45a796b3adebfa99ea0409334212f19c5e5e39b7
jrabbit.park <sk1697@naver.com>
2022-02-15 00:14:18 +0900

    불필요합 sendemail app 삭제

--- libraryEx/sendEmail/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'sendEmail'
-
-urlpatterns = [
-    path('', views.sendEmail , name='sendEmail'),
-    path('sendmail/', views.sendmail , name='sendmail')
-]
